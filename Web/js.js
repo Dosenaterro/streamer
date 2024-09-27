25deaf76d1c33475def27a5c3a72814985e19c27
Dosenaterro Arnol <159555163+Dosenaterro@users.noreply.github.com>
2024-09-27 23:19:48 +0000

    Delete Web directory

--- Web/js.js
+++
@@ -1,9 +0,0 @@
-function toggleText(className) {
-    const text = document.querySelector(`.${className}`);
-    text.style.display = text.style.display === 'block' ? 'none' : 'block';
-  }
-  
-  // Usage:
-  // <div onclick="toggleText('text1')">Toggle Text 1</div>
-  // <div class="text1" style="display: none;">Text 1 content</div>
-  
